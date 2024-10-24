912c79d96749385ddb081429f0daf16a826ea23b
MERCY <MERCY@LAPTOP-6N4NT1PU>
2024-10-24 15:56:58 +0530

    This is my java basics project

---
+++ javabasics/src/core_java_program/Demo.java
@@ -0,0 +1,17 @@
+package core_java_program;
+import java.util.*;
+public class Demo {
+
+	public static void main(String[] args) {
+//		System.out.println("Java session started....");
+//		System.out.println("By Mercy");
+		int a,b,c;
+		Scanner s =new Scanner(System.in);
+		System.out.println("Enter the values of a and b");
+		a=s.nextInt();
+		b=s.nextInt();
+		c=a+b;
+		System.out.println("Addition of a="+a+" and b="+b+" is:"+c);
+	}
+
+}
